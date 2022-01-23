5be59967088b75ce91050954814f7588a8f23142
Andrew <andrew.kapsa1@gmail.com>
2022-01-23 14:34:00 -0500

    commit tree 7d0d29db70829cd7b438e063f379f06d9d4d6177

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function superbowlWin(record) {
+    const result = record.find(isWin)
+    if(Boolean(result) === true) {
+      return result.year
+    }
+  }
+  
+  function isWin(gameObj) {
+    return gameObj.result === "W"
+  }
